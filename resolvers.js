13228dc7c35e24cc631fee48a8fb18319ea4a8b7
aramideajax <olasubomifemi98@gmail.com>
2019-01-21 12:37:54 +0100

    init push

---
+++ resolvers.js
@@ -0,0 +1,8 @@
+const Query = {
+    test: () =>{
+        var depends = "if body they hurt you";
+        return 'stress me yh?'+ depends;
+    },
+    greeting: () => 'Hello GraphQL  !!!!1'
+ }
+ module.exports = {Query}
