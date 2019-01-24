8b77491188410ab27d46174525528f43e207099d
aramideajax <olasubomifemi98@gmail.com>
2019-01-24 15:04:04 +0100

    registration and login

--- graphql-config.js
+++ graphql-config.js
@@ -1,17 +1,25 @@
-const fs = require('fs')
-const {makeExecutableSchema} = require('graphql-tools')
-const { merge } = require('lodash');
+const fs = require("fs");
+const { makeExecutableSchema } = require("graphql-tools");
+const { merge } = require("lodash");
 
-const users_schema = fs.readFileSync('./graphql/users/schema.graphql',{encoding:'utf-8'})
-const users_resolvers = require('./graphql/users/resolver')
+const users_schema = fs.readFileSync("./graphql/users/schema.graphql", {
+  encoding: "utf-8"
+});
+const users_resolvers = require("./graphql/users/resolver");
 
-const workspaces_schema = fs.readFileSync('./graphql/workspaces/schema.graphql',{encoding:'utf-8'})
-const workspaces_resolvers = require('./graphql/workspaces/resolver')
+const workspaces_schema = fs.readFileSync(
+  "./graphql/workspaces/schema.graphql",
+  { encoding: "utf-8" }
+);
+const workspaces_resolvers = require("./graphql/workspaces/resolver");
+ 
 
+var typeDefs = users_schema + workspaces_schema;
 
-var typeDefs =  users_schema+workspaces_schema
-
-var resolvers = {}; 
-const schema = makeExecutableSchema({typeDefs, resolvers: merge(resolvers,users_resolvers,workspaces_resolvers)});
+var resolvers = {};
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers: merge(resolvers, users_resolvers, workspaces_resolvers)
+});
 
 module.exports = schema;
--- graphql/users/resolver.js
+++ graphql/users/resolver.js
@@ -4,27 +4,55 @@ const Query = {
   users: () => users.find(),
 
   //resolver function for studentbyId
-  userById: (root, args, context, info) => {
-    //args will contain parameter passed in query
-    return users.find({ id: args.id });
-  }
+  userById: (root, args, context, info) => users
+                      .findOne({ 
+                        email: args.params.email,
+                        password: args.params.password
+                      }),
+  login: (root, args, context, info) => users
+                      .findOne({ 
+                        email: args.params.email,
+                        password: args.params.password
+                       }),
+  
 };
 
 // db.students.find()[0]['_id'].valueOf()
-const User = {
-
-};
+const User = {};
 const Mutation = {
-  createUser: (root, args, context, info) => {
-    users.create(
-      {
-        email: args.email
-        
-      },
-      err => console.log(err)
-    );
+  createUser: async (root, args, context, info) => {
+    var res = users.findOne({ email: args.params.email }).exec();
+    var body = await res.then(res => {
+      return res;
+    });
 
-    return users.find({ email: args.email });
+    if (body) {
+      return {
+        status: false,
+        message: "this email is already in use"
+      };
+    } else {
+      users.create(
+        {
+          email: args.params.email,
+          userName: args.params.userName,
+          fullName: args.params.fullName,
+          password: args.params.password,
+          // profilePicture: args.params.profilePicture,
+          timezone: args.params.timezone,
+          authenticationType: args.params,
+          availability: true,
+          workspaceUrls: args.params.workspaceUrls,
+          businessType: args.params.businessType,
+          role: args.params.role
+        },
+      );
+
+      return {
+        status: true,
+        message: "registration complete"
+      };
+    }
   }
 };
 module.exports = { Query, Mutation, User };
