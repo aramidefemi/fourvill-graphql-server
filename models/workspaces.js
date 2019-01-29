e6a02706a2a7dfaa390336f670235c8bbf9012b4
aramideajax <olasubomifemi98@gmail.com>
2019-01-29 14:51:31 +0100

    bug fix

--- models/workspaces.js
+++ models/workspaces.js
@@ -1,12 +1,13 @@
 let mongoose = require('mongoose')
 const user = require('./users')
+var Schema = mongoose.Schema;
 
 let workspacesSchema = new mongoose.Schema({
   url: String,
-  owner: user, 
+  owner: { type: Schema.Types.ObjectId, ref: 'user' }, 
   name: String, 
   theme: String,
-  members: [{ type: Schema.Types.ObjectId, ref: 'users' }]
+  members: [{ type: Schema.Types.ObjectId, ref: 'user' }]
 })
 
 
