a41820467e9630ac1f70bbefc38cf1f4e15dc5ba
aramideajax <olasubomifemi98@gmail.com>
2019-02-01 13:00:37 +0100

    adding spaces -- projects -- folders

--- graphql/workspaces/spaces/mutaions.js
+++ graphql/workspaces/spaces/mutaions.js
@@ -1,8 +1,23 @@
-const users = require("../../../models/users"); 
+const spaces = require("../../../models/spaces");
  
  
 const Mutation = {
- 
+    createSpace: (root, args, context, info) => spaces.create(args.params).then(res=>{
+        if(res){
+            return {
+                status: true,
+                message: 'space creation complete',
+                payload: res
+            }
+
+        }else {
+            return {
+                status: false,
+                message: 'space creation failed',
+                payload: null
+            }
+        }
+    })
 };
 module.exports = Mutation;
  
--- graphql/workspaces/spaces/queries.js
+++ graphql/workspaces/spaces/queries.js
@@ -1,7 +1,10 @@
-const users = require("../../../models/users");
+const spaces = require("../../../models/spaces");
 
 
 const Query = {
+    spaces:()=> spaces.find(),
+    spacesById: (root, args, context, info) => spaces.findOne({_id:args.id}),
+    workspaceSpaces: (root, args, context, info) => spaces.findOne({workspaces:args.id}),
 }
 
  
